2296dec6dcd6bc39bda0a88baca2064655cf1481
Tu Nombre <tu-email@ejemplo.com>
2025-10-13 21:07:06 -0700

    build(lint): add eslint + prettier with import policy and scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,34 @@
+// .eslintrc.js
 module.exports = {
   root: true,
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "@react-native-community",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier"
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2021,
-    sourceType: 'module',
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 2021, sourceType: "module" },
+  plugins: ["@typescript-eslint", "import"],
+  settings: {
+    "import/resolver": {
+      typescript: { project: "./tsconfig.json" }
+    }
   },
-  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'prettier/prettier': 'error',
-    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
-  },
-  ignorePatterns: [
-    'node_modules/',
-    'dist/',
-    'build/',
-    'android/',
-    'ios/',
-    'functions/lib/',
-    '.expo/',
-    '*.config.js',
-  ],
+    // Bloquea rutas relativas profundas: fuerza alias "@/..."
+    "no-restricted-imports": ["error", {
+      patterns: ["../*", "../../*", "../../../*", "../../../../*"]
+    }],
+    "import/order": ["warn", {
+      "groups": ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+      "newlines-between": "always",
+      "alphabetize": { order: "asc", caseInsensitive: true }
+    }],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+  }
 };
-
